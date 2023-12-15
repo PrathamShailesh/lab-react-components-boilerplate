9f1e2b81427ac5c088b38087b036a778c5fd3d2b
Pratham Shailesh Dsouza <prathamshailesh777@gmail.com>
2023-12-15 16:15:01 +0530

    new

---
+++ src/components/body.jsx
@@ -0,0 +1,16 @@
+export default function(newimg){
+    let imageData=newimg.img
+    return(
+        <div>
+        {
+          imageData.map(image=>{
+            return(
+              <div key={image.id} className="column">
+                <img src={image.img} alt=""></img>
+              </div>
+            )
+          })
+        }
+      </div>
+    )
+}
